e2c35023b39b6567379fafd9fccbbde0448a774a
Rafaela Teixeira Osawe <rafaela.osawe@gmail.com>
2021-11-24 13:15:32 +0000

    image carousel

--- js/main.js
+++ js/main.js
@@ -11,25 +11,3 @@ let stars = document.getElementById('stars');
             mountains_front.style.top = value * 0 + 'px';
         })
 
-var i = 0;
-var images = [];
-var time = 4000;
-
-images [0] = "images\desk PC.jpg";
-images [1] = 'images\gaming-newbies-stadia-videoSixteenByNineJumbo1600.jpg';
-images [2] = 'images\maxresdefault.jpg';
-images [3] = 'images\desk PC2.jpg';
-
-function changeImg(){
-    document.carousel.src = images[i];
-
-    if(i < images.length - 1){
-        i++;
-    } else {
-        i = 0;
-    }
-
-    setTimeout("changeImg()", time);
-}
-
-window.onload = changeImg;
